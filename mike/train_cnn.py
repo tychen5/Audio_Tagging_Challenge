224ceead4ad628467d76c25b1838571b3cdea04d
mike840609 <mike840609@gmail.com>
2018-05-04 23:00:23 +0800

    kfold

--- mike/train_cnn.py
+++ mike/train_cnn.py
@@ -2,26 +2,70 @@
 reference :
 https://www.kaggle.com/fizzbuzz/beginner-s-guide-to-audio-data
 '''
+import os
+import shutil
 
 import numpy as np 
+import pickle as pk 
+import pandas as pd
 
-# five fold for validation 
-X_train = np.load('data/mfcc/X_train.npy')
-X_test = np.load('data/mfcc/X_test.npy')
+from keras.utils import to_categorical
+from sklearn.model_selection import KFold 
 
-print('X_train shape : ')
-print(X_train.shape)
 
-print('X_test shape : ')
-print(X_test.shape)
+# category map dict =====================================================
+map_dict = pk.load(open('data/map.pkl' , 'rb'))
+
+
+X = np.load('data/train/train3_X.npy')
+Y_train = pd.read_csv('data/train/train3_Y.csv')
+
+
+Y_train['trans'] = Y_train['label'].map(map_dict)
+Y_train['onehot'] = Y_train['trans'].apply(lambda x: to_categorical(x,num_classes=41))
+
+Y = Y_train['onehot'].values
+
+print('X shape : ')
+print(X.shape)
+
+print('Y_train shape : ')
+print(Y_train.head(10))
+
+print('Y onehot shape :')
+print(Y.shape)
+
+#  Normalization =====================================================
+mean = np.mean(X, axis=0)
+std = np.std(X, axis=0)
+
+X = (X - mean)/std
+
+
+# five fold cross validation =====================================================
+PREDICTION_FOLDER = "predictions_2d_conv"
+if not os.path.exists(PREDICTION_FOLDER):
+    os.mkdir(PREDICTION_FOLDER)
+
+kf = KFold(n_splits=6)
+
+for train_index, test_index in kf.split(X):
+    print(train_index)
+    print(test_index)
+    print('\n')
+print('=========================================================')
 
 '''
-mean = np.mean(X_train, axis=0)
-std = np.std(X_train, axis=0)
+from sklearn.model_selection import KFold
+X = np.array([[1, 2], [3, 4], [1, 2], [3, 4],[4,5] , [3,3]])
+y = np.array([1, 2, 3, 4,5,6])
+kf = KFold(n_splits=6)
+kf.get_n_splits(X)
 
-X_train = (X_train - mean)/std
-X_test = (X_test - mean)/std
+
+for train_index, test_index in kf.split(X):
+    print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
 '''
-
-
-
